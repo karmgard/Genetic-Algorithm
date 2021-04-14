2d74e16ff706f3663c81779ee7922c9f4fc1f9c0
karmgard <dan.ubergeek@gmail.com>
2021-04-14 13:32:54 -0700

    Added fitness.h

---
+++ fitness.h
@@ -0,0 +1,14 @@
+#ifndef __FITNESS_H
+#define __FITNESS_H
+
+#include <individual.h>
+//#include <threadpool.h>
+
+void getFitness( void * );
+void outputIndividual( void * );
+void initialize_fitness_library( void );
+//void wait_for_threads( void );
+//void lock(void);
+//void unlock(void);
+
+#endif
